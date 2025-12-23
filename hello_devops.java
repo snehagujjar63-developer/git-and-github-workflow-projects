5a485841d396efda1a040479aa83c6cc515d285f
sneha gujjar <snehagujjar63@gmail.com>
2025-12-23 04:28:44 -0800

    updated java file

--- hello_devops.java
+++ hello_devops.java
@@ -1,13 +1,15 @@
- //Purpose:  Simple python program for Github practice
+ //Purpose:  Simple java Program for Github practice.
 
 public class hello_devops {
     //method to print greeting message
-    static void greet(String name){
+    static void greet(String name)//String name is parameter
+    {
         System.out.println("hello "+ name + "! Welcome to devOps learning");
     } //Main method: program execution starts here
         public static void main(String[] args){
         String userName = "Sneha";
-        greet(userName);
+        greet(userName);//pass username as argument.
+        System.out.println("Pushed to github successfully");
     }
     }
-
+//this is java program east and simple 
